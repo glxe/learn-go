0e430c5f2d6e8fec3f529ebfca6957c3a029ec9c
glxe home <gexiang0523@163.com>
2020-07-21 07:46:03 +0800

    'fix'

---
+++ basic.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var a int = 3
+var b int = 7
+var c bool = true
+var s string = "abc"
+
+func variable() {
+	var a int = 3
+	var s string = "abc"
+	var b, c int = 5, 6
+}
+
+func main() {
+	fmt.Println("Hello World!")
+
+	fmt.Println(bbb)
+}
