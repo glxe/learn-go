6e0adcfbd915ae6de95122529e16c049e589a6e1
glxe home <gexiang0523@163.com>
2020-08-25 08:05:41 +0800

    'fix'

--- basic.go
+++ basic.go
@@ -80,88 +80,104 @@ func printArray(arr [5]int) {
 func updateSlice(s []int) {
 	s[0] = 100
 }
+
+func pop(s []int) int {
+	len := len(s)
+	return s[len-1 : len][0]
+}
+
 func main() {
 	fmt.Println("Hello World!")
 
-	triangle()
+	// triangle()
 
-	fmt.Println(cpp, java, python, golang)
-	fmt.Println(b, kb, mb, gb, tb, pb)
-	if res, err := eval(4, 6, "*"); err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(res)
-	}
+	// fmt.Println(cpp, java, python, golang)
+	// fmt.Println(b, kb, mb, gb, tb, pb)
+	// if res, err := eval(4, 6, "*"); err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println(res)
+	// }
 
-	fmt.Println(apply(pow, 3, 4))
-	// OR 写成匿名函数
-	fmt.Println(apply(func(a int, b int) int {
-		return int(math.Pow(
-			float64(a), float64(b)))
-	}, 3, 4))
+	// fmt.Println(apply(pow, 3, 4))
+	// // OR 写成匿名函数
+	// fmt.Println(apply(func(a int, b int) int {
+	// 	return int(math.Pow(
+	// 		float64(a), float64(b)))
+	// }, 3, 4))
 
-	fmt.Println(sum(1, 2, 3, 4, 5))
-	fmt.Println("--------------------------------------------------")
+	// fmt.Println(sum(1, 2, 3, 4, 5))
+	// fmt.Println("--------------------------------------------------")
 
-	var arr1 [5]int
-	arr2 := [3]int{1, 2, 3}            // 如果是 := 方式赋值，则必须要制定数组里的值
-	arr3 := [...]int{9, 8, 7, 6, 5, 4} // 可以让编译器自己推断长度，但是必须加...
-	var gride [5][6]int                // 二维数组
-	fmt.Println(arr1, arr2, arr3)
-	fmt.Println(gride)
+	// var arr1 [5]int
+	// arr2 := [3]int{1, 2, 3}            // 如果是 := 方式赋值，则必须要制定数组里的值
+	// arr3 := [...]int{9, 8, 7, 6, 5, 4} // 可以让编译器自己推断长度，但是必须加...
+	// var gride [5][6]int                // 二维数组
+	// fmt.Println(arr1, arr2, arr3)
+	// fmt.Println(gride)
 
-	for i := 0; i < len(arr3); i++ {
-		fmt.Println(arr3[i])
-	}
+	// for i := 0; i < len(arr3); i++ {
+	// 	fmt.Println(arr3[i])
+	// }
 
-	for i := range arr3 {
-		fmt.Println(arr3[i])
-	}
+	// for i := range arr3 {
+	// 	fmt.Println(arr3[i])
+	// }
 
-	for i, v := range arr3 {
-		fmt.Println(i, v)
-	}
-	for _, v := range arr3 {
-		fmt.Println(v)
-	}
-	fmt.Println("--------------------------------------------------")
+	// for i, v := range arr3 {
+	// 	fmt.Println(i, v)
+	// }
+	// for _, v := range arr3 {
+	// 	fmt.Println(v)
+	// }
+	// fmt.Println("--------------------------------------------------")
 
-	printArray(arr1)
-	fmt.Println("--------------------------------------------------")
+	// printArray(arr1)
+	// fmt.Println("--------------------------------------------------")
 
-	arr := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// arr := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-	fmt.Println("arr[2:6] = ", arr[2:6])
-	fmt.Println("arr[2:] = ", arr[2:])
-	s1 := arr[2:6]
-	fmt.Println("arr[2:6] = ", s1)
-	s2 := arr[:]
-	fmt.Println("arr[:] = ", s2)
-	fmt.Println("--------------------------------------------------")
+	// fmt.Println("arr[2:6] = ", arr[2:6])
+	// fmt.Println("arr[2:] = ", arr[2:])
+	// s1 := arr[2:6]
+	// fmt.Println("arr[2:6] = ", s1)
+	// s2 := arr[:]
+	// fmt.Println("arr[:] = ", s2)
+	// fmt.Println("--------------------------------------------------")
 
-	fmt.Println("after updateSlice (s1)")
-	updateSlice(s1)
-	fmt.Println(s1)
-	fmt.Println(arr)
+	// fmt.Println("after updateSlice (s1)")
+	// updateSlice(s1)
+	// fmt.Println(s1)
+	// fmt.Println(arr)
 
-	fmt.Println("after updateSlice (s2)")
-	updateSlice(s2)
-	fmt.Println(s2)
-	fmt.Println(arr)
+	// fmt.Println("after updateSlice (s2)")
+	// updateSlice(s2)
+	// fmt.Println(s2)
+	// fmt.Println(arr)
 
-	fmt.Println("--------------------------------------------------")
+	// fmt.Println("--------------------------------------------------")
 
-	arr11 := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
-	s11 := arr11[2:6]
-	s22 := s11[3:5]
-	fmt.Printf("s11 = %v, len(s11) = %d, cap(s11) = %d\n",
-		s11, len(s11), cap(s11))
-	fmt.Printf("s22 = %v, len(s22) = %d, cap(s22) = %d\n",
-		s22, len(s22), cap(s22))
+	// arr11 := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
+	// s11 := arr11[2:6]
+	// s22 := s11[3:5]q
+	// fmt.Printf("s11 = %v, len(s11) = %d, cap(s11) = %d\n",
+	// 	s11, len(s11), cap(s11))
+	// fmt.Printf("s22 = %v, len(s22) = %d, cap(s22) = %d\n",
+	// 	s22, len(s22), cap(s22))
 
-	s33 := append(s22, 10)
-	s44 := append(s33, 11)
-	s55 := append(s44, 12)
-	fmt.Println("s33, s44, s55 =", s33, s44, s55)
-	fmt.Println("arr11 =", arr11)
+	// s33 := append(s22, 10)
+	// s44 := append(s33, 11)
+	// s55 := append(s44, 12)
+	// fmt.Println("s33, s44, s55 =", s33, s44, s55)
+	// fmt.Println("arr11 =", arr11)
+
+	s66 := [...]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	fmt.Println(pop(s66[0:]))
+
+	s77 := make([]int, 16, 32)
+	copy(s77, s66[:])
+	fmt.Println(s77)
+
+	s77 = append(s77[:8], s77[9:]...)
+	fmt.Println(s77)
 }
